58244b7176224570889e4c5ada9f22ba63a7d7a3
Your Name <1830128280@qq.com>
2020-06-30 11:19:50 +0800

    添加静态资源“

--- src/main/java/com/km/knowledgemessage/controller/PageController.java
+++ src/main/java/com/km/knowledgemessage/controller/PageController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-@RequestMapping("/")
+@RequestMapping("index")
     public String index(){
-    return "/static/static/index.html";
+    return "/static/index.html";
 }
 
 }
