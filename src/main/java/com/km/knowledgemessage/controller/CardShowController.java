99edc73fe02c39827349d2cf90718fd534902d06
wfanyue <1620888589@qq.com>
2020-07-01 00:45:15 +0800

    Update CardShowController.java

--- src/main/java/com/km/knowledgemessage/controller/CardShowController.java
+++ src/main/java/com/km/knowledgemessage/controller/CardShowController.java
@@ -19,27 +19,27 @@ public class CardShowController {
     @CrossOrigin
     @RequestMapping(value = "api/showcard")
     @ResponseBody
-    public Map<String,String> showcard(@RequestParam Long cardId, Long userId){
+    public Map<String,Object> showcard(@RequestParam Long cardId, Long userId){
 // 添加创建者，标题，标签， 添加收藏，点赞（只能一次，多次就取消），评论最后
-        HashMap<String, String> map=new HashMap<>();
+        HashMap<String, Object> map=new HashMap<>();
         try {
 // 依次显示卡片标题， 创建者， 卡片描述， 标签， 卡片内容, 。
             Card card = showCardService.getCard(cardId);
             CardNum cardNum = showCardService.getCardNum(cardId);
             String title = card.getTitle();
-            String creatorname = showCardService.showCardCreator(cardId, userId);
+            String creatorname = showCardService.showCardCreator(cardId, card.getCreatorId());
             String cardContent= card.getCardText();
             String cardDescription = card.getCardDescription();
-            String cardLable = showCardService.showCardLable(cardId, userId);
+            String cardLable = showCardService.showCardLable(cardId, card.getCreatorId());
             map.put("cardTitle", title);
             map.put("creatorName", creatorname);
             map.put("cardDescription", cardDescription);
-            map.put("labelName", cardLable);
-            map.put("cardText", cardContent);
-            map.put("gmtModified", card.getGmtModified().toString());
-            map.put("likeNum", cardNum.getLikeNum().toString());
-            map.put("commentNum", cardNum.getCommentNum().toString());
-            map.put("collectNum", cardNum.getCollectNum().toString());
+            map.put("cardLable", cardLable);
+            map.put("cardContent", cardContent);
+            map.put("modifiedTime", card.getGmtModified());
+            map.put("likeNumber", cardNum.getLikeNum());
+            map.put("commentNumber", cardNum.getCommentNum());
+            map.put("collectNumber", cardNum.getCollectNum());
             map.put("status", "true");
         }catch (Exception e){
             map.put("status", "false");
