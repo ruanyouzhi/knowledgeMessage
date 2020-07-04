adf2c1f4057dce00c82b8bba3992ec1a1049b2a6
wfanyue <1620888589@qq.com>
2020-07-04 12:03:57 +0800

    Update CardSearchService.java

--- src/main/java/com/km/knowledgemessage/service/CardSearchService.java
+++ src/main/java/com/km/knowledgemessage/service/CardSearchService.java
@@ -5,6 +5,7 @@ import com.km.knowledgemessage.Mapper.CardNumMapper;
 import com.km.knowledgemessage.Mapper.KnowledgeBaseMapper;
 import com.km.knowledgemessage.Mapper.UserMapper;
 import com.km.knowledgemessage.Model.*;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,9 +27,20 @@ public class CardSearchService {
 
     public List<Card> getSearchCard(String context) {
         CardExample cardExample = new CardExample();
-        cardExample.createCriteria().andCardDescriptionLike(context);   // 搜索相似的描述
+        CardExample allcardExample = new CardExample();
+        allcardExample.createCriteria().andTitleLike(context);
+        CardExample.Criteria cardExample_cr = cardExample.createCriteria();
+        CardExample.Criteria allcardExample_cr = allcardExample.createCriteria();
+        if(StringUtils.isNotBlank(context)){
+            //通配符%需要手动加上
+            cardExample_cr.andCardDescriptionLike("%"+context+"%");
+            allcardExample_cr.andTitleLike("%"+context+"%");
+        }
+        //cardExample.createCriteria().andCardDescriptionLike(context);   // 搜索相似的描述
         //cardExample.createCriteria().andTitleGreaterThanOrEqualTo(context);
-        List<Card> cards = cardMapper.selectByExample(cardExample);
+        List<Card> cards = cardMapper.selectByExample(allcardExample);
+        List<Card> cards1 = cardMapper.selectByExample(cardExample);
+        cards.addAll(cards1);
         return cards;
 
     }
